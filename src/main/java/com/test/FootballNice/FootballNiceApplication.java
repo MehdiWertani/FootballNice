aeeda4f0ee3b6a0d2d514c69cea54ed7e961e721
MehdiWertani <wertanimehdi@gmail.com>
2025-01-28 06:03:03 +0100

    initial commit

---
+++ src/main/java/com/test/FootballNice/FootballNiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.FootballNice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballNiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballNiceApplication.class, args);
+	}
+
+}
